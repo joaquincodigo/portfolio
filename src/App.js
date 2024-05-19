fed82efdc817aef9db9249b7cdabdd2781376b70
joaquincodigo <joaquin.codigo@gmail.com>
2024-05-19 03:03:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ProfilePhoto from './ProfilePhoto/ProfilePhoto'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img alt="hello" src="/images/ProfilePhoto.png" />
+      <ProfilePhoto />
     </div>
   );
 }
