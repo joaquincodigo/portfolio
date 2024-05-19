f00238c4e8ea8d4d34e65d779cde8ed72fe18a51
joaquincodigo <joaquin.codigo@gmail.com>
2024-05-19 05:37:44 -0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useContext } from 'react';
 import './App.css';
+
+import { LanguageProvider } from './Components/LanguageContext';
+import { ThemeProvider } from './Components/ThemeContext';
+
 import ProfilePhoto from './Components/ProfilePhoto'
+import PresentationText from './Components/PresentationText'
+import ButtonContact from './Components/ButtonContact';
 
 function App() {
+
   return (
-    <div className="App container-fluid">
-      <ProfilePhoto />
-    </div>
+    <LanguageProvider>
+      <ThemeProvider>
+        <div className="App container-fluid">
+          <ProfilePhoto />
+          <PresentationText />
+          <ButtonContact />
+        </div>
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
 
