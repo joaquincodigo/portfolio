05818c69c6abc0d0878ed5747648e43d94460d3a
joaquincodigo <joaquin.codigo@gmail.com>
2024-05-20 18:24:17 -0300

    to fix: language button icon

--- src/Components/ButtonTheme.jsx
+++ src/Components/ButtonTheme.jsx
@@ -1,12 +1,32 @@
 import React, { useContext } from 'react';
 import ThemeContext from './ThemeContext';
+import LanguageContext from './LanguageContext'
 
 const ButtonLanguage = () => {
 	const { theme, toggleTheme } = useContext(ThemeContext);
+	const { language } = useContext(LanguageContext);
+
+	let icon;
+	if (language === 'en') {
+		if (theme === 'light') {
+			const icon = "/images/ES Black Icon.svg"
+		}
+		else {
+			const icon = "/images/ES White Icon.svg"
+		}
+	}
+	else {
+		if (theme === 'light') {
+			const icon = "/images/EN Black Icon.svg"
+		}
+		else {
+			const icon = "/images/EN White Icon.svg"
+		}
+	}
 
 	return (
 		<button className="ButtonTheme btn btn-primary rounded-circle p-0" onClick={toggleTheme}>
-			<img src="/Images/US.svg" alt="US Icon" />
+			<img src={icon} alt="Language Icon" />
 		</button>
 	);
 };
