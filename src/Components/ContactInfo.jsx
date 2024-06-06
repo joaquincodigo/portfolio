79b66f7c6057f364f5ed10996f0076f4d36d0760
joaquincodigo <joaquin.codigo@gmail.com>
2024-06-06 17:26:33 -0300

    contact icon info and some formating

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -7,22 +7,53 @@ const ContactInfo = ({ isContactInfoHidden }) => {
 
   const displayStatus = isContactInfoHidden ? "hidden" : "flex";
 
+  const GITHUB_DARKBLUE_ICON = "/images/contactIcons/github_darkblue.svg";
+  const GITHUB_PALEBLUE_ICON = "/images/contactIcons/github_paleblue.svg";
+  const LINKEDIN_DARKBLUE_ICON = "/images/contactIcons/linkedin_darkblue.svg";
+  const LINKEDIN_PALEBLUE_ICON = "/images/contactIcons/linkedin_paleblue.svg";
+  const MAIL_DARKBLUE_ICON = "/images/contactIcons/mail_dark_blue.svg";
+  const MAIL_PALEBLUE_ICON = "/images/contactIcons/mail_paleblue.svg";
+  const PHONE_DARKBLUE_ICON = "/images/contactIcons/phone_darkblue.svg";
+  const PHONE_PALEBLUE_ICON = "/images/contactIcons/phone_paleblue.svg";
+
   return (
     <div
       className={`ContactInfo ${displayStatus} rounded-xl mb-6 py-3 px-3 mx-6 text-xl bg-white dark:bg-blue-90 text-blue-100 dark:text-blue-10`}
     >
       <ul>
         <li className="mb-3">
-          <img src="/images/smartphone.png" alt="phone" /> 098 577 384
+          <img
+            className="w-6 inline-block align-middle me-3"
+            src={theme === "dark" ? PHONE_PALEBLUE_ICON : PHONE_DARKBLUE_ICON}
+            alt="phone"
+          />
+          <span className="align-middle">098 577 384</span>
         </li>
         <li className="mb-3">
-          <span className="">X</span> joaquin.codigo@gmail.com
+          <img
+            className="w-6 inline-block align-middle me-3"
+            src={theme === "dark" ? MAIL_PALEBLUE_ICON : MAIL_DARKBLUE_ICON}
+            alt="mail"
+          />
+          <span className="align-middle">joaquin.codigo@gmail.com</span>
         </li>
         <li className="mb-3">
-          ><span className="">X</span> linkedin/username
+          <img
+            className="w-6 inline-block align-middle me-3"
+            src={
+              theme === "dark" ? LINKEDIN_PALEBLUE_ICON : LINKEDIN_DARKBLUE_ICON
+            }
+            alt="linkedin"
+          />
+          <span className="align-middle">linkedin/username</span>
         </li>
         <li className="mb-3">
-          <span className="">X</span> github/username
+          <img
+            className="w-6 inline-block align-middle me-3"
+            src={theme === "dark" ? GITHUB_PALEBLUE_ICON : GITHUB_DARKBLUE_ICON}
+            alt="github"
+          />
+          <span className="align-middle">github/username</span>
         </li>
       </ul>
     </div>
