c26f91c086149fdbafe2aa1904acbf11ba652df3
joaquincodigo <joaquin.codigo@gmail.com>
2024-06-07 13:20:52 -0300

    responsiveness of contact and resume buttons

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -9,7 +9,7 @@ const Button = ({ text, icon, onClick }) => {
     <div className="w-full px-6">
       <button
         onClick={onClick}
-        className="Button bg-blue-90 dark:bg-blue-70 rounded-full w-full block py-1.5 mb-3"
+        className="Button bg-blue-90 dark:bg-blue-70 rounded-full block mx-auto py-1.5 mb-3 w-full max-w-[344px] sm:w-[344px]"
       >
         <div className="flex justify-center items-center">
           <img className="me-3 w-10" src={icon} alt="Contact" />
