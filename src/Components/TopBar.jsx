962c8062a5f094ce635a2ec60dbfd08c80b45b69
joaquincodigo <joaquin.codigo@gmail.com>
2025-06-05 16:30:15 -0300

    More tweaking in the topbar

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const TopBar = ({ children }) => {
   return (
     <nav
-      className="TopBar py-1.5 shadow-sm backdrop-blur
-    bg-[linear-gradient(60deg,rgba(166,200,255,0.3),rgba(120,169,255,0.3),rgba(208,226,255,0.4))]
-    dark:bg-[linear-gradient(60deg,rgba(0,67,206,0.7),rgba(15,98,254,0.7),rgba(0,45,156,0.5))]
-
-    text-blue-90 dark:shadow-sm dark:shadow-blue-70 dark:text-cool-gray-10 fixed top-0 left-0 w-full flex items-center justify-between px-3"
+      className="TopBar py-1.5 shadow-sm backdrop-blur bg-[linear-gradient(60deg,rgba(166,200,255,0.3),rgba(120,169,255,0.3),rgba(208,226,255,0.4))] dark:bg-[linear-gradient(60deg,rgba(0,67,206,0.7),rgba(15,98,254,0.7),rgba(0,45,156,0.5))] text-blue-90 dark:shadow-md dark:shadow-blue-70 dark:text-cool-gray-10 fixed top-0 left-0 w-full flex items-center justify-between px-3"
     >
       <div className="flex flex-row items-center">
         <p className="font-bold text-lg rounded-full me-2">⬗ </p>
