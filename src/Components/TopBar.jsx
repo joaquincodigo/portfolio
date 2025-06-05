3e7106a8d7ee331fd5d2e956b16e322f0a89aebe
joaquincodigo <joaquin.codigo@gmail.com>
2025-06-05 16:07:51 -0300

    Styling for topbar in dark mode

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const TopBar = ({ children }) => {
   return (
-    <nav className="TopBar py-1.5 shadow-sm bg-cool-gray-10 text-blue-90 dark:shadow-sm dark:shadow-blue-70 dark:bg-blue-90 dark:text-cool-gray-10 fixed top-0 left-0 w-full flex items-center justify-between px-3">
+    <nav
+      className="TopBar py-1.5 shadow-sm backdrop-blur
+    bg-[linear-gradient(60deg,rgba(166,200,255,0.3),rgba(120,169,255,0.3),rgba(208,226,255,0.4))]
+    dark:bg-[linear-gradient(60deg,rgba(0,67,206,0.7),rgba(15,98,254,0.7),rgba(0,45,156,0.5))]
+
+    text-blue-90 dark:shadow-sm dark:shadow-blue-70 dark:text-cool-gray-10 fixed top-0 left-0 w-full flex items-center justify-between px-3"
+    >
       <div className="flex flex-row items-center">
         <p className="font-bold text-lg rounded-full me-2">⬗ </p>
         <p className="text-lg md:text-base">
